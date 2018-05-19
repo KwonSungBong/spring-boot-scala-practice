e471a1ac9db7f7f971795a50c43648a8b8bac14c
ksb <rnjstjdqhd3939@gmail.com>
2018-05-19 14:21:52 +0900

    test

--- src/test/scala/com/example/demo/ApplicationTests.scala
+++ src/test/scala/com/example/demo/ApplicationTests.scala
@@ -1,7 +1,9 @@
 package com.example.demo
 
+import com.example.demo.repository.TestRepository
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
@@ -9,6 +11,13 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(classOf[SpringRunner])
 @SpringBootTest
 class ApplicationTests {
+
+  var testRepository : TestRepository = null
+
+  @Autowired(required = false)
+  def setMyServiceB(testRepository : TestRepository): Unit = {
+    this.testRepository = testRepository
+  }
 
   @Test
   def index(): Unit = {
